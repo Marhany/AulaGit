f431a522c47969531ce9df7bbbc7300617db3787
marhany <maarhany@gmail.com>
2017-03-09 15:45:39 -0300

    Segunda versão.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,7 +3,8 @@ package br.univel;
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		String msg = "Olá mundo!";
+		System.out.println(msg);
 	}
 	
 }
