812d374533bbba5912d4a0e5dd7930124f94a88b
marhany <maarhany@gmail.com>
2017-03-09 16:13:52 -0300

    Commit and Push

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,9 +3,9 @@ package br.univel;
 public class Principal {
 	
 	public static void main(String[] args) {
-		String hello = "Hello ";
-		String world = "World";
-		String msg = hello + world + "!";
+		String hello = "World";
+		String world = "Hello ";
+		String msg = world + hello + "!";
 		System.out.println(msg);
 	}
 	
